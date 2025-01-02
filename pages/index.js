dda85ec45c95c204b444a31481800a1f67dbf227
JingYuan <jingyuan0926@gmail.com>
2025-01-02 18:50:22 +0800

    Added back all the data and show all concert

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home() {
   // Separate configurations for each component
   const config = {
     slider: {
-      concertIds: [8] // IDs for slider
+      concertIds: [-1] // IDs for slider
     },
     concertCard: {
-      concertIds: [8] // IDs for concert cards
+      concertIds: [-1] // IDs for concert cards
     },
     events: {
-      concertIds: [8] // IDs for events page
+      concertIds: [7,8] // IDs for events page
     }
   };
 
